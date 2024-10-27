759c1ee5232e2eeb4d5bc90eb974e1ef1f82c858
Rajohana Molotsi <rajohana.molotsi@gmail.com>
2024-10-27 21:31:09 +0200

    DONE: balance paranthesis, initialize .gitignore and .gitattributes

--- 2-stack/main.py
+++ 2-stack/main.py
@@ -9,6 +9,34 @@ def reverse_string(string):
         revString += s.pop()
     return revString
 
+def is_match(char1, char2):
+    match_dict = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    }
+    return match_dict[char1] == char2
+
+def is_balanced(string):
+    s = Stack()
+    for char in string:
+        if char == '(' or char == '[' or char == '{':
+            s.push(char)
+        elif char == ')' or char == ']' or char == '}':
+            if s.size() == 0:
+                return False
+            elif not is_match(char, s.pop()):
+                return False
+            
+    return s.size() == 0
+
 if __name__ == '__main__':
-    print(reverse_string("We will conquere COVID-19"))
-    print(reverse_string("I am the king"))
+    # print(reverse_string("We will conquere COVID-19"))
+    # print(reverse_string("I am the king"))
+
+    print(is_balanced("({a+b})"))
+    print(is_balanced("))((a+b}{"))
+    print(is_balanced("((a+b))"))
+    print(is_balanced("((a+g))"))
+    print(is_balanced("))"))
+    print(is_balanced("[a+b]*(x+2y)*{gg+kk}"))
