3e5bf7aad0329076b01fe3650690c558cd36c233
Rajohana Molotsi <rajohana.molotsi@gmail.com>
2024-10-28 18:01:51 +0200

    DONE: queue-exercise II

--- 3-queue/main.py
+++ 3-queue/main.py
@@ -1,6 +1,23 @@
 from queue import Queue
 
+def produce_binary_numbers(max):
+    q = Queue()
+    q.enqueue("1")
+
+    for _ in range(max):
+        front = q.front()
+        print(front)
+        q.enqueue(front + "0")
+        q.enqueue(front + "1")
+
+        q.dequeue()
+
+
 if __name__ == '__main__':
+
+    produce_binary_numbers(10)
+
+    '''
     q = Queue()
 
     q.enqueue({
@@ -26,3 +43,4 @@ if __name__ == '__main__':
     # print(q.dequeue())
 
     print(q.front())
+    '''
