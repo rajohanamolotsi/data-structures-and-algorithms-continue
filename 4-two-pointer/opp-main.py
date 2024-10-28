4c9b160df46a892214a1e5a5aad49ae6b5354d74
Rajohana Molotsi <rajohana.molotsi@gmail.com>
2024-10-28 19:34:10 +0200

    TRY: different tests

--- 4-two-pointer/opp-main.py
+++ 4-two-pointer/opp-main.py
@@ -4,10 +4,11 @@ if __name__ == '__main__':
     Output:  Yes
     Explanation : There is a pair (20, 50) with given target.
     '''
-    tagert = 70
-    arr = [10, 20, 35, 50]
+    tagert = 16
+    arr = [-8, 1, 4, 6, 10, 45]
+    # arr = [10, 20, 30]
     length_arr= len(arr)
-    count = 0
+    # count = 0 - you do not need this!
     for i in arr:
         for j in range(length_arr - 1, -1, -1):
             if i + arr[j] == tagert:
