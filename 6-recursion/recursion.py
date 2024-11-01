728fb513cbbeebfabcb732c29901540f1da07319
Rajohana Molotsi <rajohana.molotsi@gmail.com>
2024-11-01 13:19:42 +0200

    DONE: recursion
    Now I get it
    
    I personally think that recursion cannot be better explained without using Stack in your explaination

--- 6-recursion/recursion.py
+++ 6-recursion/recursion.py
@@ -1,12 +1,14 @@
 def factorial(n):
     if n == 0:
-        # print(f'n: {n}')
-        # print(f'n - 1: {n - 1}')
         return 1
     else:
-        # print(f'n, n - 1: {n, n - 1}')
-        print(f'factorial(n - 1): {factorial(n - 1)}')
         return n * factorial(n - 1)
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return (fibonacci(n - 1) + fibonacci(n - 2))
     
 if __name__ == '__main__':
-    factorial(3)
+    print(fibonacci(8))
