3d5548766efc7718e0b81a09c783cdab0a3fb875
Rajohana Molotsi <rajohana.molotsi@gmail.com>
2024-11-10 10:05:12 +0200

    DONE: merge, shell and quick (95%)

--- 7-dry-runs/1-dry-run.py
+++ 7-dry-runs/1-dry-run.py
@@ -2,17 +2,16 @@ def merge_sort(data):
     size = len(data)
     if size <= 1:
         return data
-    
     mid = size // 2
 
     left = data[:mid]
     right = data[mid:]
-    
+
     merge_sort(left)
     merge_sort(right)
-    merge_the_two(data, left, right)
+    merge_the_sort(data, left, right)
 
-def merge_the_two(data, left, right):
+def merge_the_sort(data, left, right):
     len_left = len(left)
     len_right = len(right)
 
@@ -39,26 +38,28 @@ def merge_the_two(data, left, right):
         k += 1
 
 def quick_sort(data, low, high):
+
     if low < high:
-        pivot_idx = partition(data, low, high)
+        pivot_idx = partition_sort(data, low, high)
         quick_sort(data, low, pivot_idx - 1)
         quick_sort(data, pivot_idx + 1, high)
 
-def partition(data, low, high):
+def partition_sort(data, low, high):
     pivot = data[low]
     leftwall = low
 
     for idx in range(low + 1, high + 1):
         if data[idx] < pivot:
-            leftwall += 1
             data[idx], data[leftwall] = data[leftwall], data[idx]
+            leftwall += 1
 
     data[low], data[leftwall] = data[leftwall], data[low]
     return leftwall
 
 def shell_sort(data):
-    size = len(data) # len(data) - 1
+    size = len(data)
     gap = size // 2
+
     while gap > 0:
         for i in range(gap, size):
             temp = data[i]
@@ -68,16 +69,19 @@ def shell_sort(data):
                 data[j] = data[j - gap]
                 j -= gap
 
-            data[j] = temp # data[i]
+            data[j] = temp
 
         gap //= 2
-
 
 if __name__ == '__main__':
 
     data = [29, 10, 14, 37, 13]
 
-    merge_sort(data)
-    # quick_sort(data, low = 0, high = len(data) - 1)
-    # shell_sort(data)
+    # merge_sort(data)
+    # low = 0
+    # high = len(data) - 1
+    # quick_sort(data, low, high)
+    # print(data)
+
+    shell_sort(data)
     print(data)
