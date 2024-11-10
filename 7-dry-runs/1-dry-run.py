144f9960d52d1a880e0b202e33bff6e7ef148a2e
Rajohana Molotsi <rajohana.molotsi@gmail.com>
2024-11-10 10:42:20 +0200

    DONE: bubble, selection and insertion

--- 7-dry-runs/1-dry-run.py
+++ 7-dry-runs/1-dry-run.py
@@ -73,15 +73,44 @@ def shell_sort(data):
 
         gap //= 2
 
+
+def bubble_sort(data):
+    size = len(data)
+    for i in range(size - 1):
+        j = i
+        swapped = False
+        for j in range(size - 1 - i):
+            if data[j] > data[j + 1]:
+                data[j], data[j + 1] = data[j + 1], data[j]
+                swapped = True
+                j -= 1 # check
+
+        if not swapped:
+            break
+
+def selection_sort(data):
+    size = len(data)
+    for i in range(size - 1): # check
+        min_idx = i
+        for j in range(min_idx + 1, size):
+            if data[j] < data[min_idx]:
+                min_idx = j
+
+        if min_idx != i:
+            data[min_idx], data[i] = data[i], data[min_idx]
+
+def insertion_sort(data):
+    size = len(data)
+    for i in range(1, size):
+        j = i
+        while j > 0 and data[j] < data[j - 1]:
+            data[j], data[j - 1] = data[j - 1], data[j]
+            j -= 1
+
+
 if __name__ == '__main__':
 
     data = [29, 10, 14, 37, 13]
 
-    # merge_sort(data)
-    # low = 0
-    # high = len(data) - 1
-    # quick_sort(data, low, high)
-    # print(data)
-
-    shell_sort(data)
+    insertion_sort(data)
     print(data)
