f9dc3c42b50957af09b8664f5cac1ef0f0f75992
Rajohana Molotsi <rajohana.molotsi@gmail.com>
2024-11-13 23:49:15 +0200

    DONE: graph

--- 11-dry-run/graph.py
+++ 11-dry-run/graph.py
@@ -1,3 +1,33 @@
+def BFS(graph_dict, start_node):
+    visited = []
+    queue = [start_node]
+
+    while queue:
+        s = queue.pop(0)
+        if s not in visited:
+            visited.append(s)
+
+            for idx in graph_dict[s]:
+                if idx not in visited:
+                    queue.append(idx)
+
+    return visited
+
+def DFS(graph_dict, start_node):
+    visited = []
+    stack = [start_node]
+
+    while stack:
+        s = stack.pop()
+        if s not in visited:
+            visited.append(s)
+
+            for idx in reversed(graph_dict[s]):
+                if idx not in visited:
+                    stack.append(idx)
+
+    return visited
+
 class Graph:
     def __init__(self):
         self.graph = {}
@@ -9,51 +39,22 @@ class Graph:
     def add_edge(self, node1, node2):
         if node1 not in self.graph:
             self.add_node(node1)
-        if node2 not in self.graph:  # Changed to `if`
+        if node2 not in self.graph:
             self.add_node(node2)
 
         self.graph[node1].append(node2)
         self.graph[node2].append(node1)
 
-    def get_neighbours(self, node):  # Fixed typo
+    def has_neighbours(self, node):
         return self.graph.get(node, [])
     
-    def has_edge(self, node1, node2):  # Minor rename for clarity
+    def has_edge(self, node1, node2):
         return node2 in self.graph.get(node1, [])
     
     def print_graph(self):
         for node in self.graph:
             print(f'{node}: {self.graph[node]}')
 
-def BFS(graph_dict, start_node):  # Accepts only the dictionary directly
-    visited = []
-    queue = [start_node]  # Initialize with start_node
-
-    while queue:
-        s = queue.pop(0)
-        if s not in visited:
-            print(s, end=' ')
-            visited.append(s)
-
-            for idx in graph_dict[s]:
-                if idx not in visited:
-                    queue.append(idx)
-
-def DFS(graph_dict, start_node):
-    visited = []
-    stack = [start_node]  # Initialize with start_node
-
-    while stack:
-        s = stack.pop()
-        if s not in visited:
-            print(s, end=' ')
-            visited.append(s)
-
-            for idx in reversed(graph_dict[s]):
-                if idx not in visited:
-                    stack.append(idx)
-
-# Example usage
 if __name__ == '__main__':
     g = Graph()
     g.add_node(1)
@@ -62,8 +63,6 @@ if __name__ == '__main__':
     g.add_edge(1, 3)
     g.add_edge(2, 4)
     g.print_graph()
-
-    print("\nBFS:")
-    BFS(g.graph, 1)
-    print("\nDFS:")
-    DFS(g.graph, 1)
+    graph_dict = g.graph
+    print(f'BFS: {BFS(graph_dict, 1)}')
+    print(f'DFS: {DFS(graph_dict, 1)}')
