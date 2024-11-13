381a22dd817d63905d6c7db3c4211f2fe5bd3143
Rajohana Molotsi <rajohana.molotsi@gmail.com>
2024-11-13 14:33:01 +0200

    DONE: Binary Search Tree

--- 10-dry-run/main.py
+++ 10-dry-run/main.py
@@ -1,33 +1,28 @@
 class BinarySearchTreeNode:
     def __init__(self, data):
-
         self.data = data
         self.left = None
         self.right = None
 
-
     def add_child(self, data):
-
         if data == self.data:
             return
-
-        if data < self.data:
+        
+        elif data < self.data:
             if self.left:
                 self.left.add_child(data)
             else:
-                self.left = self.left.add_child(data)
+                self.left = BinarySearchTreeNode(data)
         else:
             if self.right:
                 self.right.add_child(data)
             else:
-                self.right = self.right.add_child(data)
+                self.right = BinarySearchTreeNode(data)
 
     def search(self, value):
-
         if value == self.data:
             return True
-        
-        if value < self.data:
+        elif value < self.data:
             if self.left:
                 return self.left.search(value)
             else:
@@ -38,15 +33,13 @@ class BinarySearchTreeNode:
             else:
                 return False
             
-
     def delete(self, data):
-
         if data < self.data:
             if self.left:
-                return self.left.delete(data)
+                self.left = self.left.delete(data)
         elif data > self.data:
             if self.right:
-                return self.right.delete(data)
+                self.right = self.right.delete(data)
         else:
             if self.left is None and self.right is None:
                 return None
@@ -54,111 +47,50 @@ class BinarySearchTreeNode:
                 return self.right
             elif self.right is None:
                 return self.left
-            
-        min_value = self.right.find_min()
-        self.right = min_value
-        min_value = self.right.delete(min_value)
+            else:
+                min_value = self.right.find_min()
+                self.data = min_value
+                self.right = self.right.delete(min_value)
 
-
-
+        return self
+    
     def find_min(self):
         if self.left is None:
             return self.data
-        return self.left
+        return self.left.find_min()
     
     def find_max(self):
         if self.right is None:
             return self.data
-        return self.right
+        return self.right.find_max()
 
+    def in_order(self):
+        elements = []
 
-class TreeNode:
-    def __init__(self, data):
+        if self.left:
+            elements += self.left.in_order()
 
-        self.data = data
-        self.children = []
-        self.parent = None
+        elements.append(self.data)
 
-    def add_child(self, child):
-        child.parent = self
-        self.children.append(child)
+        if self.right:
+            elements += self.right.in_order()
 
-    def get_level(self):
-        level = 0
-        p = self.parent
+        return elements
 
-        while p:
-            level += 1
-            p = p.parent
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
 
-        return level
+    for idx in range(1, len(elements)):
+        root.add_child(elements[idx])
 
-    def print_tree(self):
-
-        spaces = " " * self.get_level() * 3
-        prefix = spaces + "|__" if self.parent else ""
-        print(prefix + self.data)
-
-        for child in self.children:
-            child.print_tree()
-
-class Graph:
-    def __init__(self):
-        self.graph = {}
-
-    def add_node(self, node):
-        if node in self.graph:
-            self.graph[node] = []
-
-    def add_edge(self, node1, node2):
-        if node1 not in self.graph:
-            self.add_node(node1)
-        if node2 not in self.graph:
-            self.add_node(node2)
-
-        self.graph[node1] = node2
-        self.graph[node2] = node1
-
-    def print_tree(self):
-        for node in self.graph:
-            print(f'{node}: {self.graph[node]}')
-
-    def get_neightbours(self, node):
-        if node in self.graph:
-            return self.graph.get(node, []) # chech if it is print or return
-        
-    def has_edges(self, node1, node2):
-        return node2 in self.graph.get(node1, [])
-    
+    return root
 
 if __name__ == '__main__':
-    g = Graph()
-    g.add_node(1)
-    g.add_node(2)
-    g.add_edge(1, 2)
-    g.add_edge(1, 3)
-    g.add_edge(2, 4)
-    g.print_tree()
+    elements = [17, 4, 1, 20, 9, 23, 18, 34]
 
-    root = TreeNode("Favourites")
+    r = build_tree(elements)
 
-    laptop = TreeNode("Laptop")
-    laptop.add_child(TreeNode("Samsung"))
-    laptop.add_child(TreeNode("MacBook"))
-    laptop.add_child(TreeNode("Mecer"))
-
-    phones = TreeNode("Phones")
-    phones.add_child(TreeNode("Samsung"))
-    phones.add_child(TreeNode("iPhone"))
-    phones.add_child(TreeNode("Google Pixel"))
-
-    cars = TreeNode("Cars")
-    cars.add_child(TreeNode("Mercedes-Benz"))
-    cars.add_child(TreeNode("BMW"))
-    cars.add_child(TreeNode("VW"))
-
-    root.add_child(cars)
-    root.add_child(laptop)
-    root.add_child(phones)
-
-    root.print_tree()
+    print(r.in_order())
+    r.delete(4)
+    r.delete(34)
+    print(r.in_order())
