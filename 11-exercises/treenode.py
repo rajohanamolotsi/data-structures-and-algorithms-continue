cb11f27dab6f94e2f3bc21bfa30aecee8e01b3d0
Rajohana Molotsi <rajohana.molotsi@gmail.com>
2024-11-14 12:16:50 +0200

    DONE: treenode level-exercise

--- 11-exercises/treenode.py
+++ 11-exercises/treenode.py
@@ -1,12 +1,11 @@
 class TreeNode:
-    def __init__(self, name, designation):
-        self.name = name
-        self.designation = designation
+    def __init__(self, data):
+        self.data = data
         self.children = []
         self.parent = None
 
     def get_level(self):
-        level = 0
+        level = 1
         p = self.parent
         while p:
             level += 1
@@ -17,42 +16,40 @@ class TreeNode:
         child.parent = self
         self.children.append(child)
 
-    def print_tree(self, preference):
+    def print_tree(self, lvl):
+        if self.get_level() > lvl:
+            return
         spaces = " " * self.get_level() * 3
         prefix = spaces + "|__" if self.parent else ""
-        if preference == 'name':
-            print(prefix + self.name)
-            for child in self.children:
-                child.print_tree('name')
-        elif preference == 'designation':
-            print(prefix + self.designation)
-            for child in self.children:
-                child.print_tree('designation')
-        elif preference == 'both':
-            print(f'{prefix + self.name} ({self.designation})')
-            for child in self.children:
-                child.print_tree('both')
+        print(prefix + self.data)
+        for child in self.children:
+            child.print_tree(lvl)
 
 if __name__ == '__main__':
-    root = TreeNode("Departments", "HOLY TRINITY")
+    root = TreeNode("Departments")
 
-    mathematics = TreeNode("Mathematics", "POWER")
-    mathematics.add_child(TreeNode("Linear Algebra", 'LA'))
-    mathematics.add_child(TreeNode("Multivariable Calculus", 'MC'))
-    mathematics.add_child(TreeNode("Graph Theory", 'GT'))
-    english = TreeNode("English", "COMMUNICATION")
-    english.add_child(TreeNode("Parts of Speech", 'POS'))
-    english.add_child(TreeNode("Listening", 'L'))
-    english.add_child(TreeNode("Speaking", 'SP'))
-    science = TreeNode("Science", "MIXTURE")
-    science.add_child(TreeNode("Physics", 'PHY'))
-    science.add_child(TreeNode("Chemistry", 'CHEM'))
-    science.add_child(TreeNode("Biology", 'BIO'))
+    mathematics = TreeNode("Mathematics")
+    mathematics.add_child(TreeNode("Linear Algebra"))
+    mathematics.add_child(TreeNode("Multivariable Calculus"))
+    mathematics.add_child(TreeNode("Graph Theory"))
 
+    linear = TreeNode("Linear")
+    linear.add_child(TreeNode("Chapter 1"))
+    linear.add_child(TreeNode("Chapter 3"))
+
+    english = TreeNode("English")
+    english.add_child(TreeNode("Parts of Speech"))
+    english.add_child(TreeNode("Listening"))
+    english.add_child(TreeNode("Speaking"))
+
+    science = TreeNode("Science")
+    science.add_child(TreeNode("Physics"))
+    science.add_child(TreeNode("Chemistry"))
+    science.add_child(TreeNode("Biology"))
+
+    mathematics.add_child(linear)
     root.add_child(mathematics)
     root.add_child(english)
     root.add_child(science)
 
-   #  root.print_tree('name')
-    root.print_tree('designation')
-   #  root.print_tree('both')
+    root.print_tree(4)
