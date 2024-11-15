1ee0f44c60c0003092722f825df2038ba542d5c6
Rajohana Molotsi <rajohana.molotsi@gmail.com>
2024-11-15 11:06:35 +0200

    DONE: binary search tree

---
+++ 12-exercises/binarysearchtreenode.py
@@ -0,0 +1,104 @@
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return
+        
+        elif data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+            
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+
+    def search(self, value):
+        if value == self.data:
+            return True
+        
+        elif value < self.data:
+            if self.left:
+                return self.left.search(value)
+            else:
+                return False
+        
+        elif value > self.data:
+            if self.right:
+                return self.right.search(value)
+            else:
+                return False
+            
+    def delete(self, value):
+        # check - value == self.data
+        if value < self.data:
+            if self.left:
+                self.left = self.left.delete(value)
+        elif value > self.data:
+            if self.right:
+                self.right = self.right.delete(value)
+
+        else:
+            if self.left is None and self.right is None:
+                return None
+            elif self.left is None:
+                return self.right
+            elif self.right is None:
+                return self.left
+            
+            min_value = self.right.find_min()
+            self.data = min_value
+            self.right = self.right.delete(min_value)
+
+        return self
+    
+    def in_order_traversal(self):
+        elements = []
+
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
+
+    def find_min(self):
+        if self.left:
+            return self.left.find_min()
+        return self.data
+    
+    def find_max(self):
+        if self.right:
+            return self.right.find_max()
+        return self.data
+    
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+    for data in elements[1:]:
+        root.add_child(data)
+
+    return root
+
+if __name__ == '__main__':
+    print('original_elements = [1, 4, 9, 17, 18, 20, 23, 34]')
+    numbers_tree = build_tree([17, 4, 1, 20, 9, 23, 18, 34])
+    numbers_tree.delete(20)
+    print("After deleting 20 ",numbers_tree.in_order_traversal()) # this should print [1, 4, 9, 17, 18, 23, 34]
+
+    numbers_tree = build_tree([17, 4, 1, 20, 9, 23, 18, 34])
+    numbers_tree.delete(9)
+    print("After deleting 9 ",numbers_tree.in_order_traversal())  # this should print [1, 4, 17, 18, 20, 23, 34]
+
+    numbers_tree = build_tree([17, 4, 1, 20, 9, 23, 18, 34])
+    numbers_tree.delete(17)
+    print("After deleting 17 ",numbers_tree.in_order_traversal())  # this should print [1, 4, 9, 18, 20, 23, 34]
